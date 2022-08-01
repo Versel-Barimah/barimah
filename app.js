8c1bf13a2d3b95738a5e57b138f65fab22c0cfbf
Nana Barimah <mr.barrysilas@outlook.com>
2022-08-01 22:53:52 +0000

    Modified files

---
+++ app.js
@@ -0,0 +1,11 @@
+// const fs = require('fs');
+// const express = require('express');
+
+// const app = express()
+
+// app.get('/', function(req, res){
+//         const filePath = path.join(__dirname, 'index.html');
+//         res.send(filePath);
+// })
+
+// app.listen(3000)
