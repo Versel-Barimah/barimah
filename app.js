16b75a383cd8cf19544b2a07f8b164bc8b022545
Nana Barimah <mr.barrysilas@outlook.com>
2022-08-02 13:17:22 +0000

    Mage files more dynamic with ejs template

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-// const fs = require('fs');
-// const express = require('express');
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
 
-// const app = express()
+const app = express();
 
-// app.get('/', function(req, res){
-//         const filePath = path.join(__dirname, 'index.html');
-//         res.send(filePath);
-// })
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// app.listen(3000)
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+        
+        res.render('index');
+});
+
+app.get('/projects', function(req, res) {
+        
+        res.render('projects');
+});
+
+app.get('/about', function(req, res) {
+        
+        res.render('about');
+});
+
+app.get('/contacts', function(req, res) {
+        
+        res.render('contacts');
+});
+
+
+app.listen(3000);
