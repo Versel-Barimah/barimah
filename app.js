823d70c0ab7434b5f1186a87ddc1f596178008fb
Nana Barimah <mr.barrysilas@outlook.com>
2022-08-02 21:20:31 +0000

    Adding a right port for heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 
 const app = express();
 
+const port = 3000;
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -30,4 +32,8 @@ app.get('/contacts', function(req, res) {
 });
 
 
-app.listen(3000);
+app.listen(process.env.port ||
+        
+        port, function() {
+                console.log('Server started on port ' + port);
+        });
