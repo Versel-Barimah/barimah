2fe05b04c910069d34e72216587a70916967aa76
Nana Barimah <mr.barrysilas@outlook.com>
2022-08-02 23:28:18 +0000

    Cleaning

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const path = require('path');
-const express = require('express');
-const fs = require('fs');
+// const path = require('path');
+// const express = require('express');
+// const fs = require('fs');
 
-const app = express();
+// const app = express();
 
-const port = 3000;
+// const port = 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
         
-        res.render('index');
-});
+//         res.render('index');
+// });
 
-app.get('/projects', function(req, res) {
+// app.get('/projects', function(req, res) {
         
-        res.render('projects');
-});
+//         res.render('projects');
+// });
 
-app.get('/about', function(req, res) {
+// app.get('/about', function(req, res) {
         
-        res.render('about');
-});
+//         res.render('about');
+// });
 
-app.get('/contacts', function(req, res) {
+// app.get('/contacts', function(req, res) {
         
-        res.render('contacts');
-});
+//         res.render('contacts');
+// });
 
 
-app.listen(process.env.port ||
+// app.listen(process.env.port ||
         
-        port, function() {
-                console.log('Server started on port ' + port);
-        });
+//         port, function() {
+//                 console.log('Server started on port ' + port);
+//         });
